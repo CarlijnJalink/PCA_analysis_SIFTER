112ac93388f663c88ada3303037b931cd676f4a2
Jalink <a.c.jalink@uu.nl>
2025-04-02 10:34:53 +0200

    added a function to plot PCA's

--- src/Check_output_PCA_SIFTER_method_python_course.py
+++ src/Check_output_PCA_SIFTER_method_python_course.py
@@ -4,6 +4,8 @@ Created on Tue Feb 11 14:58:33 2025
 
 @author: jalin005
 
+In this script, you can explore the PCA's that were generated for transmittence
+in the Principal Component Analysis part of the SIFTER algoritm.
 
 """
 #%% IMPORT PACKAGES
@@ -47,3 +49,29 @@ plt.xlabel("Wavelength (nm)")
 plt.ylabel("PCA Amplitude")
 plt.legend()
 plt.show() # Show the plot
+
+
+#%% FUNCTION TO PLOT PCA'S
+
+"""
+
+"""
+def plot_pca(wavelength, EOF_data, pca_index):
+    """
+    This function can be used to plot the PCA's against wavelength.
+    
+    Parameters:
+    wavelength:Array of wavelength values.
+    EOF_data: Array containing 10 generated PCA's.
+    pca_index (int): Index of the PCA to plot (starting from 0).
+    """
+    plt.plot(wavelength, EOF_data[pca_index], label=f"PCA {pca_index + 1}")
+    plt.xlabel("Wavelength (nm)")
+    plt.ylabel("PCA Amplitude")
+    plt.legend()
+    plt.show()
+
+# Example usage
+plot_pca(wavelength, EOF_data, 0)  
+plot_pca(wavelength, EOF_data, 3)  
+
